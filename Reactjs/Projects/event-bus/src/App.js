8ddd1b91bc878a5a25b53c693904d1995a17b690
tupc <tudv@newwave.vn>
2024-01-29 18:10:17 +0700

    update react demo

--- Reactjs/Projects/event-bus/src/App.js
+++ Reactjs/Projects/event-bus/src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 import { ComponentA } from './components/componentA';
 
 function App() {
+  eventBus.emit(EVENT.LOGIN, {
+    firstName: "Brian",
+    lastName: "kaylin",
+    email: "brian@robotic.com"
+  });
+
+
   return (
-    <div className="App">
-        <ComponentA />
-    </div>
+      <div className="App">
+          <ComponentA />
+      </div>
   );
 }
 
--- Reactjs/Projects/event-bus/src/components/componentB.tsx
+++ Reactjs/Projects/event-bus/src/components/componentB.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
+import { eventBus } from "../Utils/eventEmitter";
+import { EVENT } from "../utils/constants/event.constants";
 export const ComponentB = () => {
+
+    eventBus.emit(EVENT.LOGIN, {
+        firstName: "Brian",
+        lastName: "kaylin",
+        email: "brian@robotic.com"
+    });
     
     return (
-        <>Hello World</>
+        <>Start emit event</>
     )
 }
